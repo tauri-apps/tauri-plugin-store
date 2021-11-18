707ff6b92d6afbd92248797c1049162ef59ba590
Jonas Kruckenberg <iterpre@protonmail.com>
2021-11-18 17:28:09 +0100

    update example

--- examples/svelte-app/src-tauri/src/main.rs
+++ examples/svelte-app/src-tauri/src/main.rs
@@ -3,15 +3,15 @@
   windows_subsystem = "windows"
 )]
 
-use tauri_plugin_store::{Store, StoreFileBuilder};
+use tauri_plugin_store::{StoreBuilder, StorePlugin};
 
 fn main() {
-  let settings = StoreFileBuilder::new(".settings".parse().unwrap())
-  .default("the-key".to_string(), "wooooot".into())
-  .build();
+  let settings = StoreBuilder::new(".settings".parse().unwrap())
+    .default("the-key".to_string(), "wooooot".into())
+    .build();
 
   tauri::Builder::default()
-    .plugin(Store::with_stores(vec![settings]))
+    .plugin(StorePlugin::with_stores(vec![settings]))
     .run(tauri::generate_context!())
     .expect("failed to run app");
 }
