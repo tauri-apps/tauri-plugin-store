c271f038b9ee3d194fa288ef85c36b0cb882f883
Lucas Fernandes Nogueira <lucas@tauri.studio>
2023-06-17 21:56:07 +0000

    feat: update to 2.0.0-alpha.10 (#441)
    Committed via a GitHub action: https://github.com/tauri-apps/plugins-workspace/actions/runs/5300306799
    
    Co-authored-by: lucasfernog <lucasfernog@users.noreply.github.com>

--- dist-js/index.mjs
+++ dist-js/index.mjs
@@ -1,6 +1,8 @@
 import { listen } from '@tauri-apps/api/event';
 
 // Copyright 2019-2023 Tauri Programme within The Commons Conservancy
+// SPDX-License-Identifier: Apache-2.0
+// SPDX-License-Identifier: MIT
 /**
  * A key-value store persisted by the backend layer.
  */
